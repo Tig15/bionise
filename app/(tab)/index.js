7c363b97cb5f36ef7c287e619fffcfbf98694599
Tig15 <amanvgec131@gmail.com>
2023-10-24 14:53:50 +0530

    flex pox

--- app/(tab)/index.js
+++ app/(tab)/index.js
@@ -8,6 +8,7 @@ import tailwind from "twrnc";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDataRequest } from "../../redux/Action/action";
 import LocationDetector from "../../components/Locationdetector";
+import CustomSheet from "../../components/BottomSheet";
 // import SetToken from "../../components/useStorage/setToken";
 // import GetToken from "../../components/useStorage/getToken";
 
@@ -39,6 +40,7 @@ const Home = () => {
             <Categories data={featCat} />
           </View>
         )}
+        <CustomSheet />
       </View>
     </>
   );
--- components/BottomSheet/index.js
+++ components/BottomSheet/index.js
@@ -1,11 +1,28 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useMemo, useRef } from "react";
+import BottomSheet, { BottomSheetTextInput } from "@gorhom/bottom-sheet";
+import tailwind from "twrnc";
 
 const CustomSheet = () => {
+  const snapPoints = useMemo(() => ["25%", "50%", "80%"], []);
+
+  const bottomSheetRef = useRef(null);
+
   return (
-    <View>
-      <Text>CustomSheet</Text>
-    </View>
+    <BottomSheet
+      ref={bottomSheetRef}
+      index={1}
+      snapPoints={snapPoints}
+      enablePanDownToClose={false}
+      handleIndicatorStyle={{
+        backgroundColor: "skyblue",
+      }}
+      backgroundStyle={tailwind`bg-slate-600`}
+    >
+      <BottomSheetTextInput
+        style={tailwind`w-90 ml-4 mt-5 bg-slate-500 h-10 rounded-xl`}
+      />
+    </BottomSheet>
   );
 };
 
